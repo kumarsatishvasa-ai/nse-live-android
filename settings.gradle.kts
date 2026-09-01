a4042636a8ba7994fa39a0ba7e8a832e3c73c721
kumarsatishvasa-ai <kumarsatishvasa@gmail.com>
2026-09-01 18:27:42 +0530

    Create settings.gradle.kts

---
+++ settings.gradle.kts
@@ -0,0 +1,20 @@
+```kotlin
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "NSELive"
+include(":app")
+```
