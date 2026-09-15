dc60ee8f5c52468a66a148dc0228eea91fa03b6d
kumarsatishvasa-ai <kumarsatishvasa@gmail.com>
2026-09-15 21:58:34 +0530

    Update MainActivity.kt

--- app/src/main/java/com/nselive/app/MainActivity.kt
+++ app/src/main/java/com/nselive/app/MainActivity.kt
@@ -1,5 +1,5 @@
 package com.nselive.app
-
+import androidx.compose.runtime.collectAsState
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
